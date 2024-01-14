6a96a13e90fe003552f96e7d71cda416bfe4b7b5
farzadafi <farzadafi40@gmail.com>
2024-01-14 21:48:51 +0330

    create Gender class

---
+++ user_management/src/main/java/com/farzadafi/model/enumoration/Gender.java
@@ -0,0 +1,11 @@
+package com.farzadafi.model.enumoration;
+
+public enum Gender {
+    MALE("MALE"),
+    FEMALE("FEMALE"),
+    ;
+
+    Gender(final String gender) {
+        String gender1 = gender.toUpperCase();
+    }
+}
