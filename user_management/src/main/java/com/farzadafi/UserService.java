16c15ea09f1be4319fb5a3bd3e70305912466b71
farzadafi <farzadafi40@gmail.com>
2024-01-16 13:11:14 +0330

    add registerUser method

--- user_management/src/main/java/com/farzadafi/UserService.java
+++ user_management/src/main/java/com/farzadafi/UserService.java
@@ -1,4 +1,23 @@
 package com.farzadafi;
 
+import com.farzadafi.model.User;
+import com.farzadafi.model.enumoration.Role;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    public User registerUser(User user) {
+        setDefaultField(user);
+        setDefaultAvatar(user);
+        try {
+            user = repository.save(user);
+        } catch (DataIntegrityViolationException e) {
+            if (e.getMessage().contains("duplicate key value violates unique constraint"))
+                throw new UserNameDuplicateException
+                        (String.format("%s is duplicate", user.getUsername()));
+        }
+        return user;
+    }
 }
