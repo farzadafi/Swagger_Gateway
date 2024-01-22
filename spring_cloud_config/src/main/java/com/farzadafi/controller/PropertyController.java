c88033aa56302899c377ea9d06c6929c545c3c39
farzadafi <farzadafi40@gmail.com>
2024-01-22 22:38:56 +0330

    add PropertyController class

---
+++ spring_cloud_config/src/main/java/com/farzadafi/controller/PropertyController.java
@@ -0,0 +1,16 @@
+package com.farzadafi.controller;
+
+import com.farzadafi.service.PropertyService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/config")
+public class PropertyController {
+
+}
