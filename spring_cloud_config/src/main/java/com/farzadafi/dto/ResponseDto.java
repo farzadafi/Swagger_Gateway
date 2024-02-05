3ca22c8e1ca937aeafb60a977c164edc4de37106
farzadafi <farzadafi40@gmail.com>
2024-02-05 18:56:49 +0330

    add ResponseDto class

---
+++ spring_cloud_config/src/main/java/com/farzadafi/dto/ResponseDto.java
@@ -0,0 +1,15 @@
+package com.farzadafi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseDto<T> {
+    String message;
+    T data;
+}
