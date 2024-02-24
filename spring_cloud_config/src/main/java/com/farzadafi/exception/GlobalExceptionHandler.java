f8b7fc6d9775dd8f449c77aca381e304b2f64ae9
farzadafi <farzadafi40@gmail.com>
2024-02-24 04:41:15 +0330

    add handleMethodArgumentNotValid method

--- spring_cloud_config/src/main/java/com/farzadafi/exception/GlobalExceptionHandler.java
+++ spring_cloud_config/src/main/java/com/farzadafi/exception/GlobalExceptionHandler.java
@@ -1,11 +1,35 @@
 package com.farzadafi.exception;
 
+import com.farzadafi.dto.ErrorDetails;
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
 
 @ControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  @NotNull HttpHeaders headers,
+                                                                  @NotNull HttpStatusCode status,
+                                                                  @NotNull WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),
+                String
+                        .format("TotalErrors: %s First Error: %s",
+                                ex.getErrorCount(),
+                                Objects.requireNonNull(ex.getFieldError()).getDefaultMessage()),
+                400);
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
 }
