b394d665c4cb2c0c5322b994a172a7e4011ceee8
farzadafi <farzadafi40@gmail.com>
2024-03-26 19:32:41 +0330

    create ApiGatewayApplication class and add main method

---
+++ api-gateway/src/main/java/com/farzadafi/ApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.farzadafi;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@OpenAPIDefinition(info = @Info(title = "API Gateway",
+        version = "1.0",
+        description = "Documentation API Gateway v1.0"))
+@SpringBootApplication
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
