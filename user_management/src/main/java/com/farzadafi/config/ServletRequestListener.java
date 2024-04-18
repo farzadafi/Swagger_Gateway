4a4c63660738066b52d083dfdba6fdcf2dcaf6b5
farzadafi <farzadafi40@gmail.com>
2024-04-18 13:55:19 +0330

    create ServletRequestListener class

---
+++ user_management/src/main/java/com/farzadafi/config/ServletRequestListener.java
@@ -0,0 +1,10 @@
+package com.farzadafi.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ServletRequestListener implements jakarta.servlet.ServletRequestListener {
+}
