89e7d762cec005a7f32c3daecde8ed5c17966b49
farzadafi <farzadafi40@gmail.com>
2024-05-11 21:52:15 +0330

    create a jwtDocoder Bean config

--- user_management/src/main/java/com/farzadafi/config/JwtConfig.java
+++ user_management/src/main/java/com/farzadafi/config/JwtConfig.java
@@ -1,4 +1,21 @@
 package com.farzadafi.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+import java.security.interfaces.RSAPublicKey;
+
+@Configuration
 public class JwtConfig {
+
+    @Value("${jwt.public.key}")
+    RSAPublicKey publicKey;
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(this.publicKey).build();
+    }
 }
