894df81492f89192c53815b6908b25146c355beb
farzadafi <farzadafi40@gmail.com>
2024-05-12 21:20:49 +0330

    create OpenApiConfigs class

---
+++ user_management/src/main/java/com/farzadafi/config/OpenApiConfigs.java
@@ -0,0 +1,10 @@
+package com.farzadafi.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition
+@Configuration
+public class OpenApiConfigs {
+
+}
