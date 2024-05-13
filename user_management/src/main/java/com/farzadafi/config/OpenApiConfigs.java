e02ea001478d4226148eddd6d7dadcb706d59347
farzadafi <farzadafi40@gmail.com>
2024-05-13 22:03:11 +0330

    create customOpenApi config

--- user_management/src/main/java/com/farzadafi/config/OpenApiConfigs.java
+++ user_management/src/main/java/com/farzadafi/config/OpenApiConfigs.java
@@ -1,10 +1,36 @@
 package com.farzadafi.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @OpenAPIDefinition
 @Configuration
 public class OpenApiConfigs {
 
+  @Value("${openapi.service.title}")
+  String serviceTitle;
+
+  @Value("${openapi.service.version}")
+  String serviceVersion;
+
+  @Value("${openapi.service.url}")
+  String url;
+
+  @Bean
+  public OpenAPI customOpenAPI() {
+    final String securitySchemeName = "bearerAuth";
+    return new OpenAPI()
+            .servers(List.of(new Server().url(url)))
+            .components(
+                    new Components())
+            .info(new Info().title(serviceTitle).version(serviceVersion));
+  }
 }
