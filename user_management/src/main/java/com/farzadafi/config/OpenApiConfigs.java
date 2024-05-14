cd243c0e2b7bfd7b1517439d1ad811e34c5a30a5
farzadafi <farzadafi40@gmail.com>
2024-05-14 21:06:40 +0330

    add new config for jwt config

--- user_management/src/main/java/com/farzadafi/config/OpenApiConfigs.java
+++ user_management/src/main/java/com/farzadafi/config/OpenApiConfigs.java
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -30,7 +32,14 @@ public class OpenApiConfigs {
     return new OpenAPI()
             .servers(List.of(new Server().url(url)))
             .components(
-                    new Components())
+                    new Components()
+                            .addSecuritySchemes(
+                                    securitySchemeName,
+                                    new SecurityScheme()
+                                            .type(SecurityScheme.Type.HTTP)
+                                            .scheme("bearer")
+                                            .bearerFormat("JWT")))
+            .security(List.of(new SecurityRequirement().addList(securitySchemeName)))
             .info(new Info().title(serviceTitle).version(serviceVersion));
   }
 }
